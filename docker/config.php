b5c58e3313f310e41f2829de73dcbe8bc896f00a
duynn <hotboyarsenal@gmail.com>
2022-11-08 15:22:20 +0700

    redis as session handler

--- docker/config.php
+++ docker/config.php
@@ -23,6 +23,15 @@ $CFG->dataroot  = '/var/www/moodledata';
 $CFG->dirroot   = '/var/www/moodle';
 $CFG->admin     = 'admin';
 
+$CFG->session_handler_class = '\core\session\redis';
+$CFG->session_redis_host = getenv('REDIS_HOST');
+$CFG->session_redis_port = getenv('REDIS_PORT');
+$CFG->session_redis_auth = getenv('REDIS_PASS');
+$CFG->session_redis_acquire_lock_timeout = getenv('REDIS_LOCK_TIMEOUT');
+$CFG->session_redis_lock_expire = getenv('REDIS_LOCK_EXPIRE');
+$CFG->session_redis_lock_retry = getenv('REDIS_LOCK_RETRY');
+
+
 $CFG->directorypermissions = 0777;
 
 require_once(__DIR__ . '/lib/setup.php');
