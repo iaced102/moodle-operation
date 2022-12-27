58d8f305ce3972227c410c192689554317e2146c
duynn <hotboyarsenal@gmail.com>
2022-12-27 17:13:33 +0700

    init mail-worker

---
+++ moodle-operator/client/sendmail/main.go
@@ -0,0 +1,34 @@
+package client
+
+// sendmail client
+
+import (
+	"net/smtp"
+	"moodle/config"
+)
+
+
+func SendMail(to []string, subject, body string) error {
+	// Set up authentication information.
+	auth := smtp.PlainAuth(
+		"",
+		config.EMAIL,
+		config.EMAIL_PASSWORD,
+		config.SMTP,
+	)
+
+	// Connect to the server, authenticate, set the sender and recipient,
+	// and send the email all in one step.
+	err := smtp.SendMail(
+		config.SMTP+":"+config.SMTP_PORT,
+		auth,
+		config.EMAIL,
+		to,
+		[]byte(body),
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- moodle-operator/config/config.go
+++ moodle-operator/config/config.go
@@ -8,4 +8,8 @@ var (
 	PASSWORD = "MzU2ZmE3ZDM5MmUwMDFkYThiMzkwMmE3"
 	TOKEN = "gAAAAABjm_D45-Udiwl9GRqFlllTc1FRyBeywtqNDvrnxOD_Ny9orOMnSw6A9dPWdxv_wn8YWfx4pkXKTRuIQpsnaFL0mQGHEbJGVaB2x1ZRDdQM4jz8dehDExSbJts_L_tjs0l3AVJWvxijD2ek3KYb0EAB8jUDo1ZSvSK83sqZtnhaICJTf-Q"
 	INTERVAL = 5
+	EMAIL = "duynn@bizflycloud.vn"
+	EMAIL_PASSWORD = "MzU2ZmE3ZDM5MmUwMDFkYThiMzkwMmE3"
+	SMTP = "smtp.bizflycloud.vn"
+	SMTP_PORT = "587"
 )
